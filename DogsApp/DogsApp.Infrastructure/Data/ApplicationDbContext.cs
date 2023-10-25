60b9708ac3eea94e8b50afd6954afcada1584604
r-achanova <r.achanova@pmg-pernik.com>
2023-10-25 21:17:47 +0300

    upload project

---
+++ DogsApp/DogsApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DogsApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
---
+++ DogsApp/DogsApp/Controllers/DogController.cs
@@ -0,0 +1,120 @@
+﻿
+using DogsApp.Infrastructure.Data;
+using DogsApp.Infrastructure.Data.Domain;
+using DogsApp.Models.Dog;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DogController
+        public ActionResult Index()
+        {
+            List<DogAllViewModel> dogs = _context.Dogs
+                .Select(dogFromDb => new DogAllViewModel
+                {
+                    Id = dogFromDb.Id,
+                    Name = dogFromDb.Name,
+                    Age = dogFromDb.Age,
+                    Breed = dogFromDb.Breed,
+                    Picture = dogFromDb.Picture
+
+                }).ToList();
+            return View(dogs);
+        }
+
+        // GET: DogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Dog dogFromDb = new Dog
+                {
+                    Name = bindingModel.Name,
+                    Age = bindingModel.Age,
+                    Breed = bindingModel.Breed,
+                    Picture = bindingModel.Picture,
+                };
+
+                _context.Dogs.Add(dogFromDb);
+                _context.SaveChanges();
+
+                return this.RedirectToAction("Success");
+            }
+            return this.View();
+        }
+
+        public IActionResult Success()
+        {
+            return this.View();
+
+        }
+
+
+        // GET: DogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
